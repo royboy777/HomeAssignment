7043c66874c84978346c2c0c855b35aca5f83a13
royboy777 <roycanaf@hotmail.com>
2019-01-18 01:52:19 +0200

    Solution to Apache2 configuration issue of exercise3

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise3 here
+#add fix to exercise2 here
+#solution to the problem where http command failed with http 403 error - forbidden
+#problem was that the configuration of the local apache2 denied acces from any
+#the solution was to allow access to any instead
+sudo sed -i 's/deny from all/allow from all/g' /etc/apache2/sites-available/default
