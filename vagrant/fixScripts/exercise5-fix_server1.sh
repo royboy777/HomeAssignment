387dc4542ee3f84785e7893bd2781df6194b22e6
royboy777 <roycanaf@hotmail.com>
2019-01-21 19:38:28 +0200

    Solution of Password-Less Ssh over Server1 , Resolution of Exercise5-server1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -4,9 +4,6 @@
 #This is by implementing procedure for that , including : generating key in case not exist already, chmod to authorized_keys
 #Installing sshpass (in order to copy file to remote server) , copy the id_rsa.pub file to server2, adding it to server2 authorized_keys
 #Delete the id_rsa.pub on server1
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/
 chmod 600 ~/.ssh/authorized_keys
-sudo apt-get update
-sudo apt-get install sshpass
-cat ~/.ssh/id_rsa.pub | sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@192.168.100.11 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-rm ~/.ssh/id_rsa.pub
